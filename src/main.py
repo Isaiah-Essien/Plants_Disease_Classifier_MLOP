6a93d3de9bc8a49cc86f117a0349d804aa574717
Isaiah Essien(ub bobo) <i.essien@alustudent.com>
2024-11-28 01:31:30 +0200

    moved models to src

--- src/main.py
+++ src/main.py
@@ -17,9 +17,9 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 app = FastAPI()
 
 # Paths
-MODEL_PATH = os.path.join(os.path.dirname(__file__), "src/cnn_model.h5")
+MODEL_PATH = os.path.join(os.path.dirname(__file__), "models/cnn_model.h5")
 ENCODER_PATH = os.path.join(os.path.dirname(
-    __file__), "src/label_encoder.pkl")
+    __file__), "models/label_encoder.pkl")
 DATASET_PATH = os.path.join(os.path.dirname(__file__), "dataset")
 
 
